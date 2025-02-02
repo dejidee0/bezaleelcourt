627b95dc6c8f50a521c10fa54e6b69fe860a2ebc
imoleFolio <dremkay71@gmail.com>
2025-02-02 09:01:36 +0100

    build_files updated again

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,30 @@
 echo "Build script starts executing..."
 # exit on error
 set -o errexit
+# pip install -r requirements.txt
+# python manage.py collectstatic --no-input
+# python manage.py collectstatic --noinput
+
+# python manage.py migrate
+# echo "Build script executed successfully."
+
+
+echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+
+echo "Applying migrations..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
+echo "Creating output directory..."
+mkdir -p render/output
+
+echo "Moving static files..."
+cp -r staticfiles render/output/static  # Ensure 'staticfiles' is correct
+
+
 echo "Build script executed successfully."
